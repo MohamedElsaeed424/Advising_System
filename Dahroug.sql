ebaaa5619c33110824c2470fd529e92dfdca07b4
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2023-11-14 18:01:25 +0200

    Procedures_AdvisorViewPendingRequests

--- Dahroug.sql
+++ Dahroug.sql
@@ -1,4 +1,4 @@
-﻿/*W*/
+﻿/*W What to do with semester code?????*/
 Create PROC Procedures_AdvisorApproveRejectCHRequest
 	@RequestID int, 
 	@Current_semester_code varchar (40)
@@ -9,6 +9,9 @@ Create PROC Procedures_AdvisorApproveRejectCHRequest
 		UPDATE request 
 		Set status = 'accepted'
 		where @RequestID = request_id
+		UPDATE student
+		SET assigned_hours = (SELECT TOP 1 s.assigned_hours + r.credit_hours
+							from student s INNER JOIN request r on s.student_id = r.student_id AND r.request_id = @RequestID);
 	End
 	ELSE
 		UPDATE request
@@ -26,5 +29,40 @@ CREATE PROC Procedures_AdvisorViewAssignedStudents
 				INNER JOIN Course c on t.course_id = c.course_id
 				where s.advisor_id = @AdvisorID And s.major = @major);
 	GO
+/*Y               NOT FINISHED                        */
+CREATE PROC Procedures_AdvisorApproveRejectCourseRequest
+	@RequestID int,
+	@studentID int,
+	@advisorID int
+	AS
+	Declare @prereq BIT
+	SET @prereq = CASE
+		WHEN EXISTS (
+			SELECT p.prerequisite_course_id
+			FROM PreqCourse_course p
+			INNER JOIN request r ON p.course_id = r.course_id AND r.request_id = @RequestID
+			LEFT JOIN Student_Instructor_Course_Take sic ON p.prerequisite_course_id = sic.course_id AND sic.student_id = @studentID
+			WHERE sic.course_id IS NULL
+		) THEN 1
+		ELSE 0
+		END;
+	Declare @check_advisor_and_enough_hours BIT
+	SET @check_advisor_and_enough_hours  = CASE
+		WHEN EXISTS (Select * from
+			(Select student_id from student where student_id = @studentID AND advisor_id = @advisorID) s 
+			INNER JOIN Student_Instructor_Course_Take sic on s.student_id = sic.student_id INNER JOIN Course c on c.course_id = sic.course_id
 
+			) THEN 1
+			ELSE 0
+			END;
+	
+
+	GO
+/*Z*/
+CREATE PROC Procedures_AdvisorViewPendingRequests
+	@AdvisorID int 
+	AS
+	Select * from request 
+	where student_id in (Select student_id from student where advisor_id = @AdvisorID);
+	GO
 

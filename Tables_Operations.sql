a999f97ec455882d4611a619841c8c984f35b88e
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2023-11-17 16:16:53 +0200

    oli ra2yak ya saeed

--- Tables_Operations.sql
+++ Tables_Operations.sql
@@ -45,7 +45,7 @@ CREATE PROCEDURE CreateAllTables AS
 	email                 VARCHAR (40), 
 	major                 VARCHAR (40),
 	password              VARCHAR (40), 
-	financial_status      BIT ,
+	--financial_status      BIT ,
 							--AS		(SELECT          --CURRENT_TIMESTAMP > i.deadline AND i.status = 1 
 							--		CASE
 							--		WHEN CURRENT_TIMESTAMP > i.deadline AND i.status = 1 
@@ -194,6 +194,14 @@ CREATE TABLE GradPlan_Course (
 	CONSTRAINT  PK_Installment PRIMARY KEY (payment_id, deadline),
 	FOREIGN KEY (payment_id) REFERENCES Payment (payment_id) ,
 	);
+
+	ALTER TABLE Student
+	ADD financial_status AS		(SELECT          --CURRENT_TIMESTAMP > i.deadline AND i.status = 1 
+									CASE
+									WHEN CURRENT_TIMESTAMP > i.deadline AND i.status = 1 
+									THEN 1 ELSE 0 END
+									from Installment i INNER JOIN Payment p on p.payment_id = i.payment_id 
+									 AND p.student_id = Student.student_id);
 
 GO
 EXEC CreateAllTables;

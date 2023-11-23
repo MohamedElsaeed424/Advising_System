ebe23359d35377b14ad382fa9d81e282004316d6
Mahmoud Hany <88785389+MahmoudH6@users.noreply.github.com>
2023-11-23 12:21:20 +0200

    masheny keda

--- MoExec.sql
+++ MoExec.sql
@@ -1,12 +1,30 @@
 EXEC Procedures_AdminIssueInstallment
+@paymentID = 1234 ;
 EXEC Procedures_AdminDeleteCourse
+@courseID = 5678;
 EXEC Procedure_AdminUpdateStudentStatus
+@StudentID = 910 ;
 EXEC all_Pending_Requests
 EXEC Procedures_AdminDeleteSlots
+@current_semester = '7aseb keda'
 EXEC Procedures_AdvisorCreateGP
+@ID = 1234 ,
+@password = 'not password' ;
 EXEC Procedures_AdvisorAddCourseGP
-EXEC Procedures_AdvisorUpdateGP
+@expected_graduation_date = '2003-4-1' ,
+@sem_credit_hours = 789 ,
+@advisorid = 987 ,
+@studentid = 423 ;
 EXEC Procedures_AdvisorDeleteFromGP
+@student_Id = 342 ,
+@Semester_code = 'jh322' ,
+@course_name = 'lol' ;
+--next precedure will cause an error 
+--waiting for the Q&A response
+EXEC Procedures_AdvisorUpdateGP
+@student_Id = 342 ,
+@Semester_code = 'jh322' ,
+@course_name = 'lol' ;
 
 DROP PROC Procedures_AdminIssueInstallment
 DROP PROC Procedures_AdminDeleteCourse

ad7691972aa36e83a4d2e4ddf091d9ddd8548f55
Mohamed Elsaeed <mohamed.smenshawy@gmail.com>
2023-12-03 20:54:55 +0200

    #

--- EXEC_DROP_RAED.sql
+++ EXEC_DROP_RAED.sql
@@ -1,24 +1,24 @@
 ﻿DECLARE 
 	 @result INT;
 EXEC Procedures_AdvisorRegistration 
-	 @name = 'RAED',
+	 @name = 'RAEDD',
 	 @password = 'JOUMAA',
-	 @email = 'HOTMAIL',
+	 @email = 'HOTMdddwwddddAIL@gmail.com',
 	 @office = 'C6205',
 	 @advisor_id = @result OUTPUT;
 
 EXEC Procedures_AdminListStudents;
 EXEC Procedures_AdminListAdvisors;
-EXEC AdminListStudentsWithAdvisors
+EXEC AdminListStudentsWithAdvisors;
 EXEC AdminAddingSemester
 	@start_date = '2003-01-24',
 	@end_date = '2075-01-24' ,
-	@semester_code = '';
+	@semester_code = 'W23sdd3';
 EXEC Procedures_AdminAddingCourse
-	@major = '',
+	@major = 'MET',
 	@semester = 1,
 	@credit_hours = 4,
-	@name ='',
+	@name ='dodo',
 	@is_offered =0;
 EXEC Procedures_AdminLinkInstructor
 	@instructor_id = 1,
@@ -28,13 +28,13 @@ EXEC Procedures_AdminLinkStudent
 	@instructor_id = 1,
 	@student_id = 1,
 	@course_id = 1,
-	@semester_code = '';
+	@semester_code = 'W43d3';
 EXEC Procedures_AdminLinkStudentToAdvisor
 	@student_id =1,
 	@advisor_id =1;
 EXEC Procedures_AdminAddExam 
-	@Type = '',
-	@date = '',
+	@Type = 'EXAM',
+	@date = '2023-11-23' ,
 	@course_id =1;
 
 DROP PROC Procedures_AdvisorRegistration
